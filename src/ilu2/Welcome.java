4b60e6ab7c916590d79612fd12ab431347916ebc
K <100375846+Buxx0@users.noreply.github.com>
2023-04-21 14:33:16 +0200

    EX_2

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -5,7 +5,13 @@ public class Welcome {
 	public static String welcome(String input) {
 		StringBuilder welcomeMsg = new StringBuilder();
 		welcomeMsg.append("Hello, ");
-		welcomeMsg.append(input.substring(0,1).toUpperCase() + input.substring(1, input.length()));
+		if (input == null || input.trim().isEmpty()) {
+			welcomeMsg.append("my friend");
+		}
+		else {
+			welcomeMsg.append(input.substring(0,1).toUpperCase() + 
+							input.substring(1, input.length()));
+		}
 		return welcomeMsg.toString();
 	}
 
